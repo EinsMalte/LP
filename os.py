b0b021f1527f991fdb348cf662667154ae100090
EinsMalte <49271270+EinsMalte@users.noreply.github.com>
2022-07-15 15:52:42 +0200

    Version 1.2 Update

--- os.py
+++ os.py
@@ -1,1 +1,16 @@
-print "Hello world"
+import time
+import RPi.GPIO as GPIO
+
+print("OS Start")
+print("Willkommen")
+
+running = True
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)
+while(running == True):
+    GPIO.output(18, GPIO.HIGH)
+    print("LED ON")
+    time.sleep(1)
+    GPIO.output(18, GPIO.LOW)
+    print("LED OFF")
